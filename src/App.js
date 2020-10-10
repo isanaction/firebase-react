f5f6991bf2599b2dc9f0bb6c21f6e3f549ae645b
古藤勇魚 <>
2020-10-10 20:08:24 +0900

    todo 追加、削除機能実装

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
@@ -8,16 +8,40 @@ import {
   Input,
   InputGroupAddon,
   Button,
+  Table,
 } from "reactstrap";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const hundleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value);
+  };
+  const addTodo = (text) => {
+    const newTodos = [...todos, text];
+    setTodos(newTodos);
+    setValue("");
+    console.log(newTodos);
+  };
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+    console.log(newTodos);
+  };
+
   return (
     <div className="App">
       <Container>
         <h1 className="mt-4">ToDoリスト</h1>
-        <Form>
+        <Form onSubmit={hundleSubmit}>
           <InputGroup>
-            <Input type="text" value="" />
+            <Input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
             <InputGroupAddon addonType="append">
               <Button type="submit" color="primary">
                 追加
@@ -25,6 +49,23 @@ function App() {
             </InputGroupAddon>
           </InputGroup>
         </Form>
+      </Container>
+      <Container>
+        <Table>
+          <tbody>
+            {todos &&
+              todos.map((todo, index) => (
+                <tr key={index}>
+                  <th className="text-left">{todo}</th>
+                  <td className="text-right">
+                    <Button color="danger" onClick={() => removeTodo(index)}>
+                      削除
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
       </Container>
     </div>
   );
