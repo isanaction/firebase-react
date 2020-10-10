570924970ca2f6dc593398f6c01ca1de1bc6942a
古藤勇魚 <>
2020-10-10 19:18:56 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  Form,
+  InputGroup,
+  Input,
+  InputGroupAddon,
+  Button,
+} from "reactstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className="mt-4">ToDoリスト</h1>
+        <Form>
+          <InputGroup>
+            <Input type="text" value="" />
+            <InputGroupAddon addonType="append">
+              <Button type="submit" color="primary">
+                追加
+              </Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Form>
+      </Container>
     </div>
   );
 }
