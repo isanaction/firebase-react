2d2fb6f199fbf98f9e20190a3c88f6431199d35b
古藤勇魚 <>
2020-10-10 20:45:10 +0900

    ステータス追加

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     addTodo(value);
   };
   const addTodo = (text) => {
-    const newTodos = [...todos, text];
+    const newTodos = [...todos, { text, complete: false }];
     setTodos(newTodos);
     setValue("");
     console.log(newTodos);
@@ -27,6 +27,12 @@ function App() {
   const removeTodo = (index) => {
     const newTodos = [...todos];
     newTodos.splice(index, 1);
+    setTodos(newTodos);
+    console.log(newTodos);
+  };
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
     setTodos(newTodos);
     console.log(newTodos);
   };
@@ -56,8 +62,22 @@ function App() {
             {todos &&
               todos.map((todo, index) => (
                 <tr key={index}>
-                  <th className="text-left">{todo}</th>
+                  <th
+                    className="text-left"
+                    style={{
+                      textDecoration: todo.complete ? "line-through" : "",
+                    }}
+                  >
+                    {todo.text}
+                  </th>
                   <td className="text-right">
+                    <Button
+                      color={todo.complete ? "secondary" : "success"}
+                      className="mr-2"
+                      onClick={() => completeTodo(index)}
+                    >
+                      {todo.complete ? "完了" : "未完了"}
+                    </Button>
                     <Button color="danger" onClick={() => removeTodo(index)}>
                       削除
                     </Button>
